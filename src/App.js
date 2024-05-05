6f9d6ba4c6f3d3ecfd9eb7bf35d70a0f264e8523
conpc <connorroweofficial@gmail.com>
2024-05-05 13:48:39 -0500

    some changes and additions/deletions made

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { BrowserRouter, Routes, Route, NavLink} from "react-router-dom";
+import home_icon from './icons/home.png';
 
 // importing the pages
 import Home from './pages/Home'
 import About from './pages/About'
 import SignIn from './pages/SignIn'
 import Cart from './components/Cart'
-
+          
 function App() {
   return (
     <BrowserRouter>
       <nav><header class="bortillos-header">
-
-          <NavLink to = "/"><img src="iconography/home_icon.png" alt="home" /></NavLink>
-          <NavLink to ="About"><h1><i>Bortillos</i></h1></NavLink>
+          <NavLink to = "/"><img src={home_icon} alt="home"/></NavLink>
+          <NavLink to ="About"><h1><i>Bortillo's</i></h1></NavLink>
           <div id ="duo_link">
             <NavLink to ="SignIn"><button>Sign In</button></NavLink>
             <NavLink to ="Cart"><button>Cart</button></NavLink>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 export default function Home() {
     return (
         <div className="home">
+            <br/><br/><br/>
             <h2>This is a test home component</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo consequuntur, voluptas et quod magni molestiae aperiam itaque officiis? Fuga nisi reiciendis sapiente quasi dicta eius, minus, necessitatibus odio error nobis commodi, amet inventore voluptatem! Ea!
 
@@ -11,6 +12,11 @@ export default function Home() {
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo consequuntur, voluptas et quod magni molestiae aperiam itaque officiis? Fuga nisi reiciendis sapiente quasi dicta eius, minus, necessitatibus odio error nobis commodi, amet inventore voluptatem! Ea!
                 
             </p>
+
+            <button><pre id='button_text'>Start Order   >   </pre></button>
+            
+            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
         </div>
+        
     )
 }
