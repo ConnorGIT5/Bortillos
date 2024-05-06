4c3b7c44a374182b192d2c38fa73e3d7524bb0ce
conpc <connorroweofficial@gmail.com>
2024-05-05 23:57:39 -0500

    the last commit before changing the routing code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <div className="home">
-            <br/><br/><br/>
+            <br/><br/><br/><br/>
             <h2>This is a test home component</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo consequuntur, voluptas et quod magni molestiae aperiam itaque officiis? Fuga nisi reiciendis sapiente quasi dicta eius, minus, necessitatibus odio error nobis commodi, amet inventore voluptatem! Ea!
 
@@ -13,7 +13,7 @@ export default function Home() {
                 
             </p>
 
-            <button><pre id='button_text'>Start Order   >   </pre></button>
+            <button><pre id='button_text'>Start Order   {'>'}  </pre></button>
             
             <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
         </div>
