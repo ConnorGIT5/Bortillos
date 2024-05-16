0c761033ebd39d5c41c99a1d06930c0ab240004a
conpc <connorroweofficial@gmail.com>
2024-05-15 22:32:22 -0500

    compile failed due to this setting: 'Treating warnings as errors because process.env.CI = true.' so I fixed the warnings

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider} from "react-router-dom";
 
 // pages imports
-import Home from './pages/TakeoutStart'
-import LandingPage from './pages/LandingPage'
 import SignIn from './pages/SignIn'
 
 // layout imports
 import OrderRootLayout from "./layouts/OrderRootLayout";
 import Cart from './components/Cart'          
 import LandingPageLayout from "./layouts/LandingPageLayout";
-import EmptyLayout from "./layouts/EmptyLayout";
 import TakeoutStart from "./pages/TakeoutStart";
 
+// (BELOW) LandingPageLayout (index) should probably have it's own domain
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path = "/" element={<OrderRootLayout />}>
       <Route index element={<TakeoutStart />}/>
       <Route path = "SignIn" element={<SignIn />}/>
       <Route path = "Cart" element={<Cart />}/>
-      <Route path = "Index" element ={<LandingPageLayout />}> // this should go to a new domain
+      <Route path = "Index" element ={<LandingPageLayout />}> 
         <Route path ="Menu"/>
         <Route path ="Catering"/>
         <Route path ="Locations"/>
