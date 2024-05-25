c85f0c0e09f6363a37e69bdec26fe477aebbb08e
conpc <connorroweofficial@gmail.com>
2024-05-24 22:26:08 -0500

    failed to push due to zip file

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,7 +3,7 @@ export default function NotFound() {
     return (
         <div>
             <br /><br /><br />
-            <h2><pre>   404. Page not Found.</pre></h2>
+            <h2><pre>   ERROR 404: Page not Found.</pre></h2>
         </div>
     )
 
